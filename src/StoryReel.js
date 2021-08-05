4a86d7ecb016cb5da2cabf1090733e3cfd069bbd
hwihwi99 <chamgrace@naver.com>
2021-08-05 23:56:47 +0900

    StoryReel

---
+++ src/StoryReel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './StoryReel.css'
+import Story from './Story'
+
+function StoryReel() {
+    return (
+        <div className="storyreel">
+            <Story />
+        </div>
+    )
+}
+
+export default StoryReel
